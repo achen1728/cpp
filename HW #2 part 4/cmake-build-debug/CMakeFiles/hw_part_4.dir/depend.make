c86cccb46da2897355e0ea88f77ce8cb9e13a675
Andy <36139696+achen1728@users.noreply.github.com>
2018-02-25 20:01:22 -0500

    Add files via upload

---
+++ HW #2 part 4/cmake-build-debug/CMakeFiles/hw_part_4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hw_part_4.
+# This may be replaced when dependencies are built.
---
+++ HW #2 part 4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+
+    int m[100];
+
+    for (int m = 0;m <100;m++){
+        cout << m << endl;
+    }
+
+    return 0;
+}
+
