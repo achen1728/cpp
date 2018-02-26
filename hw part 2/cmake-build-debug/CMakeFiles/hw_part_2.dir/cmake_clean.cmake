1d3ddf741a6724f7ad20d160b66f1a325ce20267
Andy <36139696+achen1728@users.noreply.github.com>
2018-02-25 19:32:33 -0500

    Add files via upload

---
+++ hw part 2/cmake-build-debug/CMakeFiles/hw_part_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_part_2.dir/main.cpp.obj"
+  "hw_part_2.pdb"
+  "hw_part_2.exe"
+  "hw_part_2.exe.manifest"
+  "libhw_part_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_part_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hw part 2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+
+    int m;
+
+    cin >> m;
+
+    m = m/10;
+
+    switch(m) {
+        case 9:
+            cout << "A" << endl;
+            break;
+        case 8:
+            cout << "B" << endl;
+            break;
+        case 7:
+            cout << "C" << endl;
+            break;
+        case 6:
+            cout << "D" << endl;
+            break;
+        default:
+            cout << "F" << endl;
+    }
+    return 0;
+}
+
