e6e865fe0b8e1b99fe8fe634d3caf6a62205bac6
Andy <36139696+achen1728@users.noreply.github.com>
2018-02-25 20:02:40 -0500

    Add files via upload

---
+++ HW #2 part 6/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float multiplyAll(float F[], int size);
+        float F[5] = {1,2,3,4,5};
+}
+
+
+
+float multiplyAll(float F[], int size)
+
+{
+
+    float mul=1.0;
+
+    float multi = 0;
+
+    for (float F = 0; F < size; F++);
+        multi = multi * mul[F];
+        cout << multi << endl;
+
+
+    return mul;
+
+}
