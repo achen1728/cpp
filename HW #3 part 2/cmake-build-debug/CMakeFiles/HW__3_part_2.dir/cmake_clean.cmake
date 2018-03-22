d5303e14e879446217349909c766abc97c5e6b9a
Andy <36139696+achen1728@users.noreply.github.com>
2018-03-21 21:04:44 -0400

    Add files via upload

---
+++ HW #3 part 2/cmake-build-debug/CMakeFiles/HW__3_part_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW__3_part_2.dir/main.cpp.obj"
+  "HW__3_part_2.pdb"
+  "HW__3_part_2.exe"
+  "HW__3_part_2.exe.manifest"
+  "libHW__3_part_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW__3_part_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW #3 part 2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+#include <iomanip>
+#include <string>
+#include <cstddef>
+
+using namespace std;
+
+void printArray (const array<int, 10> &source, int a, int b){
+if (a<b){
+    cout << source[a] << " ";
+    printArray(source, a+1, b);
+    }
+}
+
+int main() {
+
+    const array <int, 10> theSource = {1,2,3,4,5,6,7,8,9};
+    int al = 0;
+    int bl = theSource.size();
+    printArray(theSource, al, bl);
+
+    return 0;
+}
